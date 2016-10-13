9ffb994a1e8dc11f6c57b1aa2038483fdeb2da1f
lnberry <lnberry@csx2.cs.ucalgary.ca>
2016-10-13 15:11:30 -0600

    got a bluetooth library, nothing runs outside of visual studios, doesn't really run in visual studios either IDEK

---
+++ C++/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include "bthcxn.cpp"
+
+using namespace std;
+
+int main(string args[])
+{
+	int num = atoi(args[1].c_str()); 		//not sure what this int is for I think it is the number of the bluetooth device
+	string name=ars[2]; 					//not sure what this string is but I think it is the name of the bluetooth device
+	cout << "Hello World!\n";
+	return 0;
+}
